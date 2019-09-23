0804ac12790d0d89220b84b11b0c25196eb4ed4b
liusiyu579 <969529245@qq.com>
2019-09-23 17:03:01 +0800

    一个函数返回参数二进制中 1 的个数

---
+++ work10.1/work10.1/10.1.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int count_one_bits(unsigned int n)
+{
+	int count = 0;
+	int i = 0;
+	for (i = 0; i < 32; i++)
+	{
+		if (((n >> i) & 1) == 1)
+		{
+			count++;
+		}
+	}
+	return count;
+}
+int main()
+{
+	int num = 0;
+	scanf("%d", &num);
+	int ret = count_one_bits(num);
+	printf("%d", ret);
+	system("pause");
+	return 0;
+}
