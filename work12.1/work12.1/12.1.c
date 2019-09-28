109f8a8fc307f1291ef71b5f78f5058f0d22fa04
liusiyu579 <969529245@qq.com>
2019-09-28 12:05:08 +0800

    输入一个数字 把他的二进制倒置 输出该值

---
+++ work12.1/work12.1/12.1.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<math.h>
+unsigned int reverse_bit(unsigned int value)
+{
+	int sum = 0;
+	int i = 0;
+	for (i = 0; i < 32; ++i)
+	{
+		int ret = ((value >> i) &1)*pow(2, 31 - i);
+		sum = sum + ret;
+	}
+	printf("%d", sum);
+}
+int main()
+{
+	int num =30;
+	reverse_bit(num);
+	system("pause");
+	return 0;
+}
