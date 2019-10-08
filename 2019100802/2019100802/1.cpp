4ec5e7d1f9d7c04b78ff9e41703de25b19f29b7d
liusiyu579 <969529245@qq.com>
2019-10-08 14:34:14 +0800

    初始化栈 入栈 出栈 取栈顶元素 判空

---
+++ 2019100802/2019100802/1.cpp
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+#define MAXSIZE 100
+typedef int Status;
+#define OK 1;
+#define ERROR 0;
+typedef char ElemType;
+typedef struct
+{
+	ElemType*top;
+	ElemType*base;
+	int stacksize;
+}Sqstack;
+Status InitStack(Sqstack &S)
+{
+	S.base = new ElemType[MAXSIZE];
+	if (!S.base) exit(OVERFLOW);
+	S.top = S.base;
+	S.stacksize = MAXSIZE;
+	return OK;
+}
+int StackEmpty(Sqstack S)
+{
+	if (S.top == S.base)
+	{
+		return OK;
+	}
+	else
+	{
+		return ERROR;
+	}
+}
+Status Push(Sqstack &S, ElemType e)
+{
+	if (S.top - S.base == S.stacksize)
+	{
+		return ERROR;
+	}
+	*S.top++ = e;
+	return OK;
+}
+Status Pop(Sqstack &S, ElemType &e)
+{
+	if (S.top==S.base)
+	{
+		return ERROR;
+	}
+	e = *(--S.top);
+	return OK;
+}
+ElemType GetTop(Sqstack S)
+{
+	if (S.top != S.base)
+	{
+		return *(S.top - 1);
+	}
+}
+int main()
+{
+	Sqstack sq;
+	int i = 0;
+	InitStack(sq);
+	ElemType elem;
+	for (i = 0; i < 5; i++)
+	{
+		cin >> elem;
+		Push(sq, elem);
+	}
+	cout << "Õ»¶¥ÔªËØÎª£º";
+	cout << GetTop(sq) << endl;
+	for (i = 0; i < 5; i++)
+	{
+		Pop(sq, elem);
+		cout << elem << endl;
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}
